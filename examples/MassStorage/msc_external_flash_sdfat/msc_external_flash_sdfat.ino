8aebfbe37640846bbbaaef39010e9106d0bb2ce2
hathach <thach@tinyusb.org>
2019-06-16 00:45:51 +0700

    msc update to use flash block API (with caching)

--- examples/MassStorage/msc_external_flash_sdfat/msc_external_flash_sdfat.ino
+++ examples/MassStorage/msc_external_flash_sdfat/msc_external_flash_sdfat.ino
@@ -15,28 +15,16 @@ const int chipSelect = 10;
 const int spi_freq_mhz = 50;
 
 #if defined(__SAMD51__) || defined(NRF52840_XXAA)
-  #include "Adafruit_QSPI.h"
-  #include "Adafruit_QSPI_Flash.h"
-
-  Adafruit_QSPI_Flash flash;
+  Adafruit_FlashTransport_QSPI flashTransport(PIN_QSPI_SCK, PIN_QSPI_CS, PIN_QSPI_IO0, PIN_QSPI_IO1, PIN_QSPI_IO2, PIN_QSPI_IO3);
 #else
-  // Configuration of the flash chip pins and flash fatfs object.
-  // You don't normally need to change these if using a Feather/Metro
-  // M0 express board.
-
-  // Flash chip type. If you change this be sure to change the fatfs to match as well
-  #define FLASH_TYPE     SPIFLASHTYPE_W25Q16BV
-
   #if (SPI_INTERFACES_COUNT == 1)
-    #define FLASH_SS       SS                    // Flash chip SS pin.
-    #define FLASH_SPI_PORT SPI                   // What SPI port is Flash on?
+    Adafruit_FlashTransport_SPI flashTransport(SS0, &SPI);
   #else
-    #define FLASH_SS       SS1                    // Flash chip SS pin.
-    #define FLASH_SPI_PORT SPI1                   // What SPI port is Flash on?
+    Adafruit_FlashTransport_SPI flashTransport(SS1, &SPI1);
   #endif
-
-  Adafruit_SPIFlash flash(FLASH_SS, &FLASH_SPI_PORT);     // Use hardware SPI
 #endif
+
+Adafruit_SPIFlash flash(&flashTransport);
 
 Adafruit_USBD_MSC usb_msc;
 SdFat sd;
@@ -48,11 +36,7 @@ void setup()
   usb_msc.setMaxLun(2);
 
   //------------- Lun 0 for external flash -------------//
-#if defined(__SAMD51__) || defined(NRF52840_XXAA)
   flash.begin();
-#else
-  flash.begin(FLASH_TYPE);
-#endif
   usb_msc.setID(0, "Adafruit", "External Flash", "1.0");
   usb_msc.setCapacity(0, flash.pageSize()*flash.numPages()/512, 512);
   usb_msc.setReadWriteCallback(0, external_flash_read_cb, external_flash_write_cb, external_flash_flush_cb);
@@ -108,7 +92,6 @@ void sdcard_flush_cb (void)
   sd.card()->syncBlocks();
 }
 
-
 //--------------------------------------------------------------------+
 // External Flash callbacks
 //--------------------------------------------------------------------+
@@ -118,8 +101,9 @@ void sdcard_flush_cb (void)
 // return number of copied bytes (must be multiple of block size)
 int32_t external_flash_read_cb (uint32_t lba, void* buffer, uint32_t bufsize)
 {
-  const uint32_t addr = lba*512;
-  flash_cache_read((uint8_t*) buffer, addr, bufsize);
+  // Note: SPIFLash Bock API: readBlocks/writeBlocks/syncBlocks
+  // already include 4K sector caching internally. We don't need to cache it, yahhhh!!
+  flash.readBlocks(lba, (uint8_t*) buffer, bufsize/512);
   return bufsize;
 }
 
@@ -128,9 +112,9 @@ int32_t external_flash_read_cb (uint32_t lba, void* buffer, uint32_t bufsize)
 // return number of written bytes (must be multiple of block size)
 int32_t external_flash_write_cb (uint32_t lba, uint8_t* buffer, uint32_t bufsize)
 {
-  // need to erase & caching write back
-  const uint32_t addr = lba*512;
-  flash_cache_write(addr, buffer, bufsize);
+  // Note: SPIFLash Bock API: readBlocks/writeBlocks/syncBlocks
+  // already include 4K sector caching internally. We don't need to cache it, yahhhh!!
+  flash.writeBlocks(lba, buffer, bufsize/512);
   return bufsize;
 }
 
@@ -138,109 +122,5 @@ int32_t external_flash_write_cb (uint32_t lba, uint8_t* buffer, uint32_t bufsize
 // used to flush any pending cache.
 void external_flash_flush_cb (void)
 {
-  flash_cache_flush();
-}
-
-
-//--------------------------------------------------------------------+
-// Flash Caching for External Flash
-//--------------------------------------------------------------------+
-#define FLASH_CACHE_SIZE          4096        // must be a erasable page size
-#define FLASH_CACHE_INVALID_ADDR  0xffffffff
-
-uint32_t cache_addr = FLASH_CACHE_INVALID_ADDR;
-uint8_t  cache_buf[FLASH_CACHE_SIZE];
-
-static inline uint32_t page_addr_of (uint32_t addr)
-{
-  return addr & ~(FLASH_CACHE_SIZE - 1);
-}
-
-static inline uint32_t page_offset_of (uint32_t addr)
-{
-  return addr & (FLASH_CACHE_SIZE - 1);
-}
-
-void flash_cache_flush (void)
-{
-  if ( cache_addr == FLASH_CACHE_INVALID_ADDR ) return;
-
-  // indicator
-  digitalWrite(LED_BUILTIN, HIGH);
-
-  flash.eraseSector(cache_addr/FLASH_CACHE_SIZE);
-  flash.writeBuffer(cache_addr, cache_buf, FLASH_CACHE_SIZE);
-
-  digitalWrite(LED_BUILTIN, LOW);
-
-  cache_addr = FLASH_CACHE_INVALID_ADDR;
-}
-
-uint32_t flash_cache_write (uint32_t dst, void const * src, uint32_t len)
-{
-  uint8_t const * src8 = (uint8_t const *) src;
-  uint32_t remain = len;
-
-  // Program up to page boundary each loop
-  while ( remain )
-  {
-    uint32_t const page_addr = page_addr_of(dst);
-    uint32_t const offset = page_offset_of(dst);
-
-    uint32_t wr_bytes = FLASH_CACHE_SIZE - offset;
-    wr_bytes = min(remain, wr_bytes);
-
-    // Page changes, flush old and update new cache
-    if ( page_addr != cache_addr )
-    {
-      flash_cache_flush();
-      cache_addr = page_addr;
-
-      // read a whole page from flash
-      flash.readBuffer(page_addr, cache_buf, FLASH_CACHE_SIZE);
-    }
-
-    memcpy(cache_buf + offset, src8, wr_bytes);
-
-    // adjust for next run
-    src8 += wr_bytes;
-    remain -= wr_bytes;
-    dst += wr_bytes;
-  }
-
-  return len - remain;
-}
-
-void flash_cache_read (uint8_t* dst, uint32_t addr, uint32_t count)
-{
-  // overwrite with cache value if available
-  if ( (cache_addr != FLASH_CACHE_INVALID_ADDR) &&
-       !(addr < cache_addr && addr + count <= cache_addr) &&
-       !(addr >= cache_addr + FLASH_CACHE_SIZE) )
-  {
-    int dst_off = cache_addr - addr;
-    int src_off = 0;
-
-    if ( dst_off < 0 )
-    {
-      src_off = -dst_off;
-      dst_off = 0;
-    }
-
-    int cache_bytes = min(FLASH_CACHE_SIZE-src_off, count - dst_off);
-
-    // start to cached
-    if ( dst_off ) flash.readBuffer(addr, dst, dst_off);
-
-    // cached
-    memcpy(dst + dst_off, cache_buf + src_off, cache_bytes);
-
-    // cached to end
-    int copied = dst_off + cache_bytes;
-    if ( copied < count ) flash.readBuffer(addr + copied, dst + copied, count - copied);
-  }
-  else
-  {
-    flash.readBuffer(addr, dst, count);
-  }
+  flash.syncBlocks();
 }
