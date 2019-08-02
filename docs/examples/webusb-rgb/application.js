e34b1314457e94c1c3c822cbb4fc75a02a6320e7
hathach <thach@tinyusb.org>
2019-08-02 20:54:13 +0700

    convert hmlt utf16 to utf8

--- docs/examples/webusb-rgb/application.js
+++ docs/examples/webusb-rgb/application.js
@@ -10,24 +10,13 @@
       port.connect().then(() => {
         statusDisplay.textContent = '';
         connectButton.textContent = 'Disconnect';
+
+        port.onReceiveError = error => {
+          console.error(error);
+        };
       }, error => {
         statusDisplay.textContent = error;
       });
-    }
-
-    function str2ab(str) {
-      var buf = new ArrayBuffer(str.length); // 2 bytes for each char
-      var bufView = new Uint8Array(buf);
-      for (var i=0, strLen=str.length; i<strLen; i++) {
-        bufView[i] = str.charCodeAt(i);
-      }
-      return buf;
-    }
-
-    function hexToBytes(hex) {
-      for (var bytes = [], c = 0; c < hex.length; c += 2)
-      bytes.push(parseInt(hex.substr(c, 2), 16));
-      return bytes;
     }
 
     connectButton.addEventListener('click', function() {
@@ -56,11 +45,10 @@
       }
     });
 
-
     let colorPicker = document.getElementById("color_picker");
 
     colorPicker.addEventListener("change", function(event) {
-      port.send(str2ab(colorPicker.value));
+      port.send(new TextEncoder("utf-8").encode(colorPicker.value));
     });
   });
 })();
