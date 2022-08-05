357e656681dc749ff8a2f89b27fc16b48b23a6fe
hathach <thach@tinyusb.org>
2022-08-05 19:10:03 +0700

    multiple cdc example: check for CFG_TUD_CDC to be at least 2
    add note that esp32-sx is not supported yet (more works needed).

--- examples/CDC/cdc_multi/cdc_multi.ino
+++ examples/CDC/cdc_multi/cdc_multi.ino
@@ -15,22 +15,45 @@
  *
  * Requirement:
  *  The max number of CDC ports (CFG_TUD_CDC) has to be changed to at least 2.
- *  Config file is located in Adafruit_TinyUSB_Arduino/src/arduino/ports/platform/tusb_config_platform.h
- *  where platform is one of: esp32, nrf, rp2040, samd
+ *  Config file is located in Adafruit_TinyUSB_Arduino/src/arduino/ports/{platform}/tusb_config_{platform}.h
+ *  where platform is one of: nrf, rp2040, samd
+ *
+ *  NOTE: Currnetly multiple CDCs on ESP32-Sx is not yet supported.
+ *  An PR to update core/esp32/USBCDC and/or pre-built libusb are needed.
+ *  We would implement this later when we could.
  */
 
 #include <Adafruit_TinyUSB.h>
 
 #define LED LED_BUILTIN
 
-// Create extra USB Serial Ports.  "Serial" is already created.
-Adafruit_USBD_CDC USBSer1;
+// Create 2nd instance of CDC Ports.
+#ifdef ARDUINO_ARCH_ESP32
+  #error "Currnetly multiple CDCs on ESP32-Sx is not yet supported. An PR to update core/esp32/USBCDC and/or pre-built libusb are needed."
+  // for ESP32, we need to specify instance number when declaring object
+  Adafruit_USBD_CDC USBSer1(1);
+#else
+  Adafruit_USBD_CDC USBSer1;
+#endif
 
 void setup() {
   pinMode(LED, OUTPUT);
 
-  // start up all of the USB Vitual ports, and wait for them to enumerate.
   Serial.begin(115200);
+
+  // check to see if multiple CDCs are enabled
+  if ( CFG_TUD_CDC < 2 ) {
+    digitalWrite(LED, HIGH); // LED on for error indicator
+
+    while(1) {
+      Serial.printf("CFG_TUD_CDC must be at least 2, current value is %u\n", CFG_TUD_CDC);
+      Serial.println("  Config file is located in Adafruit_TinyUSB_Arduino/src/arduino/ports/{platform}/tusb_config_{platform}.h");
+      Serial.println("  where platform is one of: nrf, rp2040, samd");
+      delay(1000);
+    }
+  }
+
+  // initialize 2nd CDC interface
   USBSer1.begin(115200);
 
   while (!Serial || !USBSer1) {
