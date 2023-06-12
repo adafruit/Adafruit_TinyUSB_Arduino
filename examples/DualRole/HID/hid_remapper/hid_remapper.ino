3a2ee4e480ba17ede38e1bf78ba0c526c9ada35c
hathach <thach@tinyusb.org>
2023-06-12 23:29:06 +0700

    add more note for pico-w

--- examples/DualRole/HID/hid_remapper/hid_remapper.ino
+++ examples/DualRole/HID/hid_remapper/hid_remapper.ino
@@ -107,14 +107,16 @@ void setup1() {
   pio_cfg.pin_dp = PIN_USB_HOST_DP;
 
 #if defined(ARDUINO_RASPBERRY_PI_PICO_W)
-  /* https://github.com/sekigon-gonnoc/Pico-PIO-USB/issues/46 */
+  // For pico-w, PIO is also used to communicate with cyw43
+  // Therefore we need to alternate the pio-usb configuration
+  // details https://github.com/sekigon-gonnoc/Pico-PIO-USB/issues/46
   pio_cfg.sm_tx      = 3;
   pio_cfg.sm_rx      = 2;
   pio_cfg.sm_eop     = 3;
   pio_cfg.pio_rx_num = 0;
   pio_cfg.pio_tx_num = 1;
   pio_cfg.tx_ch      = 9;
-#endif /* ARDUINO_RASPBERRY_PI_PICO_W */
+#endif
 
   USBHost.configure_pio_usb(1, &pio_cfg);
 
