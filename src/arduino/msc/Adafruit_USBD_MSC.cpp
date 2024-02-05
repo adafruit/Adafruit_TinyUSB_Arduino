15111810b0feeb1ed05d421afc945f8a78e60570
hathach <thach@tinyusb.org>
2024-02-05 11:30:12 +0700

    clean up msc class

--- src/arduino/msc/Adafruit_USBD_MSC.cpp
+++ src/arduino/msc/Adafruit_USBD_MSC.cpp
@@ -32,48 +32,9 @@
 
 static Adafruit_USBD_MSC *_msc_dev = NULL;
 
-#ifdef ARDUINO_ARCH_ESP32
-static uint16_t msc_load_descriptor(uint8_t *dst, uint8_t *itf) {
-  // uint8_t str_index = tinyusb_add_string_descriptor("TinyUSB MSC");
-  // uint8_t str_index = 0;
-
-  uint8_t ep_in = tinyusb_get_free_in_endpoint();
-  uint8_t ep_out = tinyusb_get_free_out_endpoint();
-  TU_VERIFY(ep_in && ep_out);
-  ep_in |= 0x80;
-
-  uint16_t const desc_len =
-      _msc_dev->makeItfDesc(*itf, dst, TUD_MSC_DESC_LEN, ep_in, ep_out);
-  *itf += 1;
-  return desc_len;
-}
-#endif
-
 Adafruit_USBD_MSC::Adafruit_USBD_MSC(void) {
   _maxlun = 1;
   memset(_lun_info, 0, sizeof(_lun_info));
-
-#ifdef ARDUINO_ARCH_ESP32
-  // ESP32 requires setup configuration descriptor on declaration
-  _msc_dev = this;
-  tinyusb_enable_interface(USB_INTERFACE_MSC, TUD_MSC_DESC_LEN,
-                           msc_load_descriptor);
-#endif
-}
-
-uint16_t Adafruit_USBD_MSC::makeItfDesc(uint8_t itfnum, uint8_t *buf,
-                                        uint16_t bufsize, uint8_t ep_in,
-                                        uint8_t ep_out) {
-  uint8_t const desc[] = {
-      TUD_MSC_DESCRIPTOR(itfnum, _strid, ep_out, ep_in, EPSIZE)};
-  uint16_t const len = sizeof(desc);
-
-  if (bufsize < len) {
-    return 0;
-  }
-  memcpy(buf, desc, len);
-
-  return len;
 }
 
 uint16_t Adafruit_USBD_MSC::getInterfaceDescriptor(uint8_t itfnum_deprecated,
@@ -90,7 +51,16 @@ uint16_t Adafruit_USBD_MSC::getInterfaceDescriptor(uint8_t itfnum_deprecated,
   uint8_t const ep_in = TinyUSBDevice.allocEndpoint(TUSB_DIR_IN);
   uint8_t const ep_out = TinyUSBDevice.allocEndpoint(TUSB_DIR_OUT);
 
-  return makeItfDesc(itfnum, buf, bufsize, ep_in, ep_out);
+  uint8_t const desc[] = {
+      TUD_MSC_DESCRIPTOR(itfnum, _strid, ep_out, ep_in, EPSIZE)};
+  uint16_t const len = sizeof(desc);
+
+  if (bufsize < len) {
+    return 0;
+  }
+  memcpy(buf, desc, len);
+
+  return len;
 }
 
 void Adafruit_USBD_MSC::setMaxLun(uint8_t maxlun) { _maxlun = maxlun; }
