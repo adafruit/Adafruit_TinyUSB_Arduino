e12a2f80f86ba5416a42b7eb8b314c9455efe676
hathach <thach@tinyusb.org>
2024-02-20 14:00:54 +0700

    fix CFG_TUD_ENDPOINT0_SIZE typo

--- src/arduino/ports/nrf/tusb_config_nrf.h
+++ src/arduino/ports/nrf/tusb_config_nrf.h
@@ -63,7 +63,7 @@ extern "C" {
 // DEVICE CONFIGURATION
 //--------------------------------------------------------------------
 
-#define CFG_TUD_ENDOINT0_SIZE 64
+#define CFG_TUD_ENDPOINT0_SIZE 64
 
 //------------- CLASS -------------//
 #define CFG_TUD_CDC 1
